8543a8f3b52e2a1228d782cc3e769529b78591c3
diogo-adao <diogoadao01x@gmail.com>
2024-09-17 18:58:44 +0100

    testing mlx functions

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -6,18 +6,65 @@
 /*   By: diolivei <diolivei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 15:04:40 by diolivei          #+#    #+#             */
-/*   Updated: 2024/09/05 16:41:47 by diolivei         ###   ########.fr       */
+/*   Updated: 2024/09/17 18:54:59 by diolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int main(int argc, char *argv[])
+int on_destroy(t_data *data)
 {
-	char *str = argv[1];
-	if (argc == 2)
-		ft_printf("%s", str);
-	else
-		ft_printf("error!");
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	ft_printf("Game Ended");
+	free(data->mlx_ptr);
+	exit(0);
+	return (0);
+}
+
+int on_keypress()
+{
+	static int i = -1;
+	if (i == -1)
+	{
+		ft_printf("Game Started!\n");
+		i++;
+	}
+	else if (i >= 0)
+	{
+		i++;
+		ft_printf("You made %d moves\n", i);
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	t_data data;
+	int img_width = 256;
+	int img_height = 256;
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (1);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 256, 256, "hi :)");
+	if (!data.win_ptr)
+		return (free(data.mlx_ptr), 1);
+	data.textures[0] = mlx_xpm_file_to_image(data.mlx_ptr, FLOOR, &img_width, &img_height);
+	if (!data.textures[0])
+	{
+		ft_printf("Error: Failed to load XPM file: %s\n", FLOOR);
+		return (free(data.win_ptr), free(data.mlx_ptr), 1);
+	}
+
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], 0, 0);
+
+	// Register key release hook
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
+
+	// Register destroy hook
+	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
+
+	// Loop over the MLX pointer
+	mlx_loop(data.mlx_ptr);
 	return (0);
 }
--- so_long/src/so_long.h
+++ so_long/src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: diolivei <diolivei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 16:39:58 by diolivei          #+#    #+#             */
-/*   Updated: 2024/09/05 16:41:38 by diolivei         ###   ########.fr       */
+/*   Updated: 2024/09/17 18:23:46 by diolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,16 @@
 
 # include "../libs/libft/libft.h"
 # include "../libs/mlx/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+# define FLOOR "assets/floor.xpm"
+
+typedef struct s_data
+{
+	void		*mlx_ptr; // MLX pointer
+	void		*win_ptr; // MLX window pointer
+	void		*textures[5]; // MLX image pointers (on the stack)
+}	t_data;
 
 #endif
