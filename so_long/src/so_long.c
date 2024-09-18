9d51e7085d2942f5f43ddd02d59da26b50e69511
diogo-adao <diogoadao01x@gmail.com>
2024-09-18 18:35:01 +0100

    map design

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: diolivei <diolivei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 15:04:40 by diolivei          #+#    #+#             */
-/*   Updated: 2024/09/17 18:54:59 by diolivei         ###   ########.fr       */
+/*   Updated: 2024/09/18 18:23:06 by diolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,22 +41,59 @@ int on_keypress()
 int	main(void)
 {
 	t_data data;
-	int img_width = 256;
-	int img_height = 256;
+	int img_width = 48;
+	int img_height = 48;
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
 		return (1);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 256, 256, "hi :)");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 336, 240, "Mazeland");
 	if (!data.win_ptr)
 		return (free(data.mlx_ptr), 1);
 	data.textures[0] = mlx_xpm_file_to_image(data.mlx_ptr, FLOOR, &img_width, &img_height);
-	if (!data.textures[0])
-	{
-		ft_printf("Error: Failed to load XPM file: %s\n", FLOOR);
-		return (free(data.win_ptr), free(data.mlx_ptr), 1);
-	}
+	data.textures[1] = mlx_xpm_file_to_image(data.mlx_ptr, WALL, &img_width, &img_height);
+	data.textures[2] = mlx_xpm_file_to_image(data.mlx_ptr, PLAYER, &img_width, &img_height);
+	data.textures[3] = mlx_xpm_file_to_image(data.mlx_ptr, COLLECT, &img_width, &img_height);
+	data.textures[4] = mlx_xpm_file_to_image(data.mlx_ptr, EXIT, &img_width, &img_height);
 
-	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], 0, 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 0, SIZE * 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 1, SIZE * 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 2, SIZE * 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 3, SIZE * 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 4, SIZE * 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 5, SIZE * 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 6, SIZE * 0);
+
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 0, SIZE * 1);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[2], SIZE * 1, SIZE * 1);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], SIZE * 2, SIZE * 1);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[3], SIZE * 3, SIZE * 1);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 4, SIZE * 1);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[4], SIZE * 5, SIZE * 1);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 6, SIZE * 1);
+
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 0, SIZE * 2);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], SIZE * 1, SIZE * 2);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 2, SIZE * 2);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 3, SIZE * 2);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], SIZE * 4, SIZE * 2);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], SIZE * 5, SIZE * 2);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 6, SIZE * 2);
+
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 0, SIZE * 3);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], SIZE * 1, SIZE * 3);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], SIZE * 2, SIZE * 3);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[3], SIZE * 3, SIZE * 3);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], SIZE * 4, SIZE * 3);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 5, SIZE * 3);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 6, SIZE * 3);
+
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 0, SIZE * 4);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 1, SIZE * 4);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 2, SIZE * 4);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 3, SIZE * 4);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 4, SIZE * 4);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 5, SIZE * 4);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], SIZE * 6, SIZE * 4);
 
 	// Register key release hook
 	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
--- so_long/src/so_long.h
+++ so_long/src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: diolivei <diolivei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 16:39:58 by diolivei          #+#    #+#             */
-/*   Updated: 2024/09/17 18:23:46 by diolivei         ###   ########.fr       */
+/*   Updated: 2024/09/18 17:26:17 by diolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,29 @@
 # include <X11/X.h>
 # include <X11/keysym.h>
 
-# define FLOOR "assets/floor.xpm"
+# define SIZE 48
+# define FLOOR "textures/floor.xpm"
+# define WALL "textures/wall.xpm"
+# define PLAYER "textures/ghost.xpm"
+# define COLLECT "textures/pumpkin.xpm"
+# define EXIT "textures/exit.xpm"
+
+typedef struct s_map
+{
+	char	**map;
+	int		width;
+	int		height;
+	int		player;
+	int		collectible;
+	int		exit;
+}	t_map;
 
 typedef struct s_data
 {
 	void		*mlx_ptr; // MLX pointer
 	void		*win_ptr; // MLX window pointer
 	void		*textures[5]; // MLX image pointers (on the stack)
+	t_map		map;
 }	t_data;
 
 #endif
